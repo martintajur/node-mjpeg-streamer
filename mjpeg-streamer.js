128778470a3e8bc28edc4f876592201858e6dd19
Martin Tajur <martin@tajur.ee>
2016-11-10 11:21:06 +0200

    getting the picture working in VLC et al, making the library work under Node 7.x

--- mjpeg-streamer.js
+++ mjpeg-streamer.js
@@ -75,7 +75,7 @@ var server = http.createServer(function(req, res) {
         console.log("requested " + req.url);
         var boundary = "BOUNDARY";
         res.writeHead(200, {
-            'Content-Type': 'multipart/x-mixed-replace;boundary="' + boundary + '"',
+            'Content-Type': 'multipart/x-mixed-replace;boundary=' + boundary + '',
             'Connection': 'keep-alive',
             'Expires': 'Fri, 01 Jan 1990 00:00:00 GMT',
             'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
@@ -88,7 +88,7 @@ var server = http.createServer(function(req, res) {
             res.write('Content-Type: image/jpeg\r\n');
             res.write('Content-Length: ' + data.length + '\r\n');
             res.write("\r\n");
-            res.write(Buffer(data, 'binary'));
+            res.write(new Buffer(data, 'binary'));
             res.write("\r\n");
 
         });
